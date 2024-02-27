d344e0327843b2f96b2ef4f42bffb582849376f7
Allex Ortiz <allexortiz@outlook.com>
2024-02-26 22:29:35 -0600

    fixed functionality for login/logout and storing the token

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // import Logo from '../assets/images/Placeholder.jpg';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
+console.log(Auth);
 
 const Navbar = () => {
   return (
@@ -10,7 +12,19 @@ const Navbar = () => {
       </li>
       <li><Link to='/workouts'>Workouts</Link></li>
       <li><Link to='/why-us'>Why Us</Link></li>
-      <li><Link to='/login'>Login</Link></li>
+      <div>
+        {Auth.loggedIn() ? (
+          <div>
+            {/* <li>Welcome User!</li> */}
+            <button onClick={Auth.logout}>Logout</button>
+          </div>
+        ) : (
+          <div>
+            {/* <li>Welcome Guest!</li> */}
+            <Link to='/logIn'>Log In</Link>
+          </div>
+        )}
+      </div>
     </ul>
   )
 }

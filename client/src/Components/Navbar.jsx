5c8b77cc74c21a8cb4f712873a9b3c9500a31230
Allex Ortiz <allexortiz@outlook.com>
2024-02-27 22:16:00 -0600

    fixed style for navbar in Navbar.jsx and Header.css files

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,31 +1,34 @@
+// import { useState } from 'react';
 import React from 'react';
 // import Logo from '../assets/images/Placeholder.jpg';
 import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
 console.log(Auth);
 
+// const [username, setName] = useState('');
+
 const Navbar = () => {
   return (
-    <ul className='navbar'>
-      <li>
-        <Link to='/'>Home</Link>
-      </li>
-      <li><Link to='/workouts'>Workouts</Link></li>
-      <li><Link to='/why-us'>Why Us</Link></li>
+    <div className='navbar'>
+      <div>
+        <Link to='/' class="nav-link bg-orange-500 focus:bg-blue-700 text-white rounded-full p-1">Home</Link>
+      </div>
+      <div><Link to='/workouts' class="nav-link bg-orange-500 focus:bg-blue-700 text-white rounded-full p-1">Workouts</Link></div>
+      <div><Link to='/why-us' class="nav-link bg-orange-500 focus:bg-blue-700 text-white rounded-full p-1">Why Us</Link></div>
       <div>
         {Auth.loggedIn() ? (
           <div>
             {/* <li>Welcome User!</li> */}
-            <button onClick={Auth.logout}>Logout</button>
+            <button onClick={Auth.logout} class='button-override nav-link bg-orange-500 focus:bg-blue-700 text-white rounded-full p-1'>Logout</button>
           </div>
         ) : (
           <div>
             {/* <li>Welcome Guest!</li> */}
-            <Link to='/logIn'>Log In</Link>
+            <Link to='/logIn' class="nav-link bg-orange-500 focus:bg-blue-700 text-white rounded-full p-1">Log In</Link>
           </div>
         )}
       </div>
-    </ul>
+    </div>
   )
 }
 

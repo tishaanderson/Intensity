cd4b878747c9981b4015cf725a8b7a70cacd759b
Allex Ortiz <allexortiz@outlook.com>
2024-02-29 00:27:45 -0600

    deleted unnecessary files

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { ADD_USER } from '../utils/mutations'
 import {useMutation} from '@apollo/client'
-import '../styles/SignUp.css'
 import Auth from '../utils/auth'
 
 const SignUp = () => {
